5159be36234497d0f8312564d10fb1e60596481c
Дарья <hzxz4610@gmail.com>
2024-11-20 15:43:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-day = int(input("Введите день: ")) # Ввод значения
-month = int(input("Введите месяц: "))  # Ввод значения
-if not (1 <= month <= 12 and 1 <= day <= 31):
-    print("Неверная дата")
-elif 3 <= month <= 5:
-    print("Весна")
-elif 6 <= month <= 8:
-    print("Лето")
-elif 9 <= month <= 11:
-    print("Осень")
-else:
-    print("Зима")
-
+while True:
+    day = int(input("Введите день: ")) # Ввод значения
+    month = int(input("Введите месяц: "))  # Ввод значения
+    if day < 32 and month < 13:
+        if 3 <= month <= 5:
+            print("Весна")
+            break
+        elif 6 <= month <= 8:
+            print("Лето")
+            break
+        elif 9 <= month <= 11:
+            print("Осень")
+            break
+        else:
+            print("Зима")
+            break
